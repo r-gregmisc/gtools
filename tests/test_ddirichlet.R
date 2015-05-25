9c28e728c048ff30d917978a3b07cb1226cdf942
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2015-05-25 14:30:24 +0000

    Add library call.

--- tests/test_ddirichlet.R
+++ tests/test_ddirichlet.R
@@ -1,5 +1,4 @@
-## Bug reported by John Nolan:
-
+## Regression test for bug reported by John Nolan:
 ##
 ## Whenever the pair (x[i], a[i]) == (0,1), NA would be returned, due
 ## to an internal computation of ( 0 * -Inf ) => NaN
@@ -7,6 +6,8 @@
 ## The code now checks for this particular issue and sets the value of
 ## ( 0 * -Inf ) to 0, which is correct for this calculation.
 ##
+library(gtools)
+
 x = c(0,0,1)
 alpha = c(1,2,3)
 
