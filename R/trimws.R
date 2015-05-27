e5c07dbe43311281df8fa430fa93147d41b89fb3
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2015-05-27 16:38:37 +0000

    Fix missing closing paren.

--- R/trimws.R
+++ R/trimws.R
@@ -1,6 +1,6 @@
 ## trimws was added in R 2.3.0.  If we're using a previous version of
 ## R we need to define it.
-if(!exists('trimws', mode='function')
+if(!exists('trimws', mode='function'))
    trimws <-  function(s)
                 {
                   s <- sub(pattern="^[[:blank:]]+", replacement="", x=s)
