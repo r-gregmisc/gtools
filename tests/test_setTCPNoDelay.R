fc1b20f4d94d4646c312472772c79a7719e56276
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2016-08-15 19:14:49 +0000

    Add test code for setTCPNoDelay

---
+++ tests/test_setTCPNoDelay.R
@@ -0,0 +1,44 @@
+library('gtools')
+
+setTCPNoDelay <- function( socket, value=TRUE )
+{
+  if(!any(c("socket","sockconn") %in% class(socket)))
+    stop("socket must be a socket object")
+
+  buffer <- paste(rep(" ", 1000), sep='', collapse='')
+
+  if("sockconn" %in% class(socket))
+    conn <- getConnection(socket[1])
+  else
+    conn <- socket
+
+
+  retval <- .C("R_setTCPNoDelay",
+               socket=as.integer(socket[1]),
+               flag=as.integer(value),
+               status=integer(1),
+               status.str=as.character(buffer),
+               status.len=as.integer(nchar(buffer)),
+               package="gtools"
+  )
+
+  if(retval$status != 0)
+    stop( retval$status.str )
+  else
+    invisible(retval$status.str)
+}
+
+
+
+host <- "rdgas05026.am.boehringer.com"
+#host <- "www.r-project.org"
+socket <- make.socket(host, 80)
+print(socket)
+setTCPNoDelay(socket, TRUE)
+
+write.socket(socket, "GET /\n\n")
+while( (str <- read.socket(socket)) > "")
+  cat(str)
+close.socket(socket)
+
+read.socket(socket)
