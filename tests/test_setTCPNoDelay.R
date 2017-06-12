2d81a31218b936e475015bcee1938cc889d5657b
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2017-06-12 22:52:41 +0000

    Add improved example to man page for setTCPnoDelat() and fix test function.

--- tests/test_setTCPNoDelay.R
+++ tests/test_setTCPNoDelay.R
@@ -30,14 +30,14 @@ setTCPNoDelay <- function( socket, value=TRUE )
 
 
 
-host <- "rdgas05026.am.boehringer.com"
-#host <- "www.r-project.org"
+host <- "www.r-project.org"
 socket <- make.socket(host, 80)
 print(socket)
 setTCPNoDelay(socket, TRUE)
 
 write.socket(socket, "GET /\n\n")
-write.socket(socket, "A\0B\n")
+write.socket(socket, "A")
+write.socket(socket, "B\n")
 while( (str <- read.socket(socket)) > "")
   cat(str)
 close.socket(socket)
