54f00633ceca51a6f8fc39ecc086a29e892e95b6
Gregory R Warnes <greg@warnes.net>
2021-05-25 18:22:40 -0400

    Correct bug in `lastAdd` specifying that the existing `.Last` should be of mode `function`.

--- R/lastAdd.R
+++ R/lastAdd.R
@@ -4,15 +4,17 @@
 lastAdd <- function( fun )
   {
     if (!is.function(fun)) stop("fun must be a function")
-    if(!exists(".Last", envir=.GlobalEnv))
+  
+    if(!exists(".Last", envir=.GlobalEnv, mode="function"))
       {
         return(fun)
       }
     else
       {
-        Last <- get(".Last", envir=.GlobalEnv)
+        Last <- get(".Last", envir=.GlobalEnv, mode="function")
         newfun <- function(...)
           {
+            browser()
             fun()
             Last()
           }
