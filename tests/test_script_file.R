3428723b8a0c6dd0073560f0f1adacb59a6b09fa
Gregory R Warnes <greg@warnes.net>
2021-05-31 13:53:39 -0400

    Handle windows path separator when testing `script_file` and `script_path`.

--- tests/test_script_file.R
+++ tests/test_script_file.R
@@ -1,14 +1,17 @@
 library(gtools)
 
-# cmdArgs <- commandArgs(trailingOnly = FALSE)
-# print(cmdArgs)
+# For debugging conveneince
+cmdArgs <- commandArgs(trailingOnly = FALSE)
+print(cmdArgs)
 
-file <- script_file("warning")
-# cat("script_file:", file, "\n")
+file <- script_file("warning") 
+file <- gsub("\\\\", "/", file)
+cat("script_file:", file, "\n")
 stopifnot(endsWith(file, "/tests/test_script_file.R"))
 
 path <- script_path("warning")
-# cat("script_path:", path, "\n")
+path <- gsub("\\\\", "/", path)
+cat("script_path:", path, "\n")
 stopifnot(endsWith(path, "/tests"))
 
 # save.image("test_script_file.Rda")
